dccca2ec112acbc5007fe83a5d5c7ea538e203b5
taltekc <taltekc@gmail.com>
2019-03-17 12:36:29 -0400

    Update LoginFacebook.java

--- src/test/facebook/login/LoginFacebook.java
+++ src/test/facebook/login/LoginFacebook.java
@@ -1,23 +1,37 @@
 package test.facebook.login;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginFacebook {
-
+	
+	
 	public static void main(String[] args) {
 		
+		// Tell the code where is your driver located
 		System.setProperty("webdriver.chrome.driver", "C:/Users/TalentTEK/Documents/GitHub/Framework2019/drivers/chromedriver.exe");
+		// init the chrome driver
 		WebDriver driver = new ChromeDriver();
+		// lets go to facebook.com
 		driver.get("http://www.facebook.com");
+		// maximize the window 
 		driver.manage().window().maximize();
+		// wait 30 second to wait all elements to load in the page
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
-		driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("MyFirstName");
-		driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("LName");
+		// Locators
+		String fnLoc = "//input[@name='firstname']";
+		String lnLoc = "//input[@name='lastname']";
+		// Values
+		String fnVal = "fnTest";
+		String lnVal = "lnTest";
+		
+		// Enter firstName
+		driver.findElement(By.xpath(fnLoc)).sendKeys(fnVal);
+		// Enter lastName
+		driver.findElement(By.xpath(lnLoc)).sendKeys(lnVal);
 
 	}
 
