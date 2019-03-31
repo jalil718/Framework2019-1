f5659739f726777fa0a996e8d0346a79c4bf0010
taltekc <taltekc@gmail.com>
2019-03-31 11:47:47 -0400

    SKIP TEST

---
+++ src/prec/PriorityPrec.java
@@ -0,0 +1,30 @@
+package prec;
+
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class PriorityPrec {
+
+	
+	@Test (priority=2)
+	public void changePass(){
+		System.out.println ("Changing password");
+	}
+	
+	@Test (priority=3)
+	public void logOutTest(){
+		System.out.println ("Log out first");
+	}
+	
+	@Test (priority=1)
+	public void logInTest(){
+		System.out.println ("Login first");
+	}
+	
+	@Test (priority=4)
+	public void goinToSkipTest(){
+		System.out.println ("Skipped this test");
+		throw new SkipException ("SF-1: requirement changed so skipping it");
+	}
+	
+}
