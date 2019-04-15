2853a0c9812fca0453fa46f70019fb12dc80e026
taltekc <taltekc@gmail.com>
2019-04-14 20:37:26 -0400

    read and write in xl

--- src/prec/ReadFromXl.java
+++ src/prec/ReadFromXl.java
@@ -8,24 +8,25 @@ import locators.facebookLocators;
 
 public class ReadFromXl extends Config{
 
-	Xls_Reader xl = new Xls_Reader (System.getProperty("user.dir")+"//tc.xlsx");
+	Xls_Reader xl = new Xls_Reader (System.getProperty("user.dir")+"//testCase.xlsx");
 	facebookLocators fl = new facebookLocators();
 	
 	@Test
 	public void testReadFromXL(){
-		//Xls_Reader xl = new Xls_Reader ("C:\\Users\\Hasan\\workspace//TestNG_Framework\\data.xlsx");
-		// dynamic path - locators
-		String value = xl.getCellData("fb", "Values", 2);
+		//String value = xl.getCellData(sheetName, colName, rowNum)
+		String value = xl.getCellData("facebook", "TestData", 2);
+		System.out.println("value" + value);
 		driver.findElement(By.xpath(fl.email)).sendKeys(value);
-		driver.findElement(By.xpath(fl.fnLoc)).sendKeys(xl.getCellData("fb", "Values", 3));
+		driver.findElement(By.xpath(fl.fnLoc)).sendKeys(xl.getCellData("facebook", "TestData", 3));
 	}
 	
 	@Test
 	public void testWriteInXl(){
 		// Copy the text from fb website and enter in excel file
 		String getTextValue = driver.findElement(By.xpath(fl.signUpText)).getText();
-		String value = xl.setCellData("fb", "Values", 4, "");
-		driver.findElement(By.xpath(fl.fnLoc)).sendKeys(xl.setCellData("fb", "Values", 4, ""));
+		System.out.println("Text from fb website : "+ getTextValue);
+		//driver.findElement(By.xpath(fl.fnLoc)).sendKeys(xl.setCellData("fb", "Values", 4, getTextValue));
+		xl.setCellData("facebook", "TestData", 4, getTextValue);
 	}
 	
 	
